8504d1a19c2a64f51cee75c68a86c62637e41fd8
leodebuss <bionerd729@gmail.com>
2026-04-03 20:57:04 -0700

    hashcode, getters, and setters for shelf done, need more still and testing too!

--- src/main/java/Utilities/Shelf.java
+++ src/main/java/Utilities/Shelf.java
@@ -30,4 +30,40 @@ public class Shelf {
     }
 
     // Getters and Setters
+    public int getShelfNumber() {
+        return shelfNumber;
+    }
+
+    public void setShelfNumber(int shelfNumber) {
+        this.shelfNumber = shelfNumber;
+    }
+
+    public String getSubject() {
+        return subject;
+    }
+
+    public void setSubject(String subject) {
+        this.subject = subject;
+    }
+
+    public HashMap<Book, Integer> getBooks() {
+        return books;
+    }
+
+    public void setBooks(HashMap<Book, Integer> books) {
+        this.books = books;
+    }
+
+    // equals & hashCode (DO NOT use books)
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (!(o instanceof Shelf)) {
+            return false;
+        }
+        Shelf shelf = (Shelf) o;
+        return shelfNumber == shelf.shelfNumber && Objects.equals(subject, shelf.subject);
+    }
 }
